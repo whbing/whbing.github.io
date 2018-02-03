e3549ec24329ed71dc0c1403b45e101c22c703dd
whbing <284734261@qq.com>
2018-02-03 23:08:50 +0800

    Site updated: 2018-02-03 23:08:42

--- js/addfun.js
+++ js/addfun.js
@@ -3,6 +3,20 @@
 /*这个主要是文档加载后 增加样式 在animate.js中也写的有*/
 $(function() {
     $("body").addClass("loaded");
+    if($(document).width() >= 1280){
+        $("#mobile").remove();
+
+    }else{
+        $("#pc").remove();
+
+    }
+});
+$(window).resize(function() {
+    if($(document).width() >= 1280){
+        $("#mobile").remove();
+    }else{
+        $("#pc").remove();
+    }
 });
 /*以下这个初始化时wow.js*/
 new WOW().init();
